927f0f3332a07556d3ae925f6d0430650e1986d0
Marco F <marco.fariselli@greenwaves-technologies.com>
2021-02-18 18:32:53 +0100

    resolved conflicts

--- squeezenet.c
+++ squeezenet.c
@@ -10,56 +10,28 @@
 #include <stdio.h>
 
 #ifndef __EMUL__
-/* PMSIS includes. */
-#include "pmsis.h"
-#endif  /* __EMUL__ */
-
+  /* PMSIS includes. */
+  #include "pmsis.h"
+#endif
 #include "squeezenet.h"
-#include "squeezenetInfo.h"
 #include "squeezenetKernels.h"
 #include "gaplib/ImgIO.h"
 
 #define __XSTR(__s) __STR(__s)
 #define __STR(__s) #__s
 
-#ifdef __EMUL__
-#ifdef PERF
-#undef PERF
-#endif
-#endif
-
-#define AT_INPUT_WIDTH 224
-#define AT_INPUT_HEIGHT 224
-#define AT_INPUT_COLORS 3
-
 #define AT_INPUT_SIZE (AT_INPUT_WIDTH*AT_INPUT_HEIGHT*AT_INPUT_COLORS)
-#define AT_OUTPUT_SIZE 1001
-
-
-//This is defined in the Makefile, in case it is not we define it here
-#ifndef STACK_SIZE
-#define STACK_SIZE     2048 
-#endif
+#define NUM_CLASSES 1001
 
 AT_HYPERFLASH_FS_EXT_ADDR_TYPE __PREFIX(_L3_Flash) = 0;
-
-#ifdef __EMUL__
-  #include <sys/types.h>
-  #include <unistd.h>
-  #include <sys/stat.h>
-  #include <fcntl.h>
-  #include <sys/param.h>
-  #include <string.h>
-  #ifndef TENSOR_DUMP_FILE
-    #define TENSOR_DUMP_FILE "tensor_dump_file.dat"
-  #endif
-#endif
 
 // Softmax always outputs Q15 short int even from 8 bit input
 L2_MEM short int *ResOut;
 typedef unsigned char IMAGE_IN_T;
-L2_MEM IMAGE_IN_T *ImageIn;
-
+char *ImageName;
+#ifdef __EMUL__
+  L2_MEM IMAGE_IN_T *S0_Input_1;
+#endif
 
 static void RunNetwork()
 {
@@ -70,7 +42,11 @@ static void RunNetwork()
   gap_cl_resethwtimer();
 #endif
 
-  __PREFIX(CNN)(ImageIn, ResOut);
+#ifdef __EMUL__
+  __PREFIX(CNN)(S0_Input_1, ResOut);
+#else
+  __PREFIX(CNN)(ResOut);
+#endif
   printf("Runner completed\n");
 
   printf("\n");
@@ -78,101 +54,97 @@ static void RunNetwork()
   //Checki Results
   int max_class=0;
   int max_value=0;
-  for (int i=0;i<AT_OUTPUT_SIZE;i++){
+  for (int i=0;i<NUM_CLASSES;i++){
     if(ResOut[i]>max_value){
       max_value=ResOut[i];
       max_class=i;
     }
   }
 
-  printf("Class detected: %d, with value: %f\n", max_class, max_value*S39_Op_output_1_OUT_SCALE);
+  printf("Class detected: %d, with value: %d\n", max_class, max_value);
+  //FIX2FP(max_value,15);
   printf("\n");
 }
 
 int start()
 {
-  char *ImageName = __XSTR(AT_IMAGE);
-  printf("image name %s \n", __XSTR(AT_IMAGE));
-  struct pi_device cluster_dev;
-  struct pi_cluster_task *task;
-  struct pi_cluster_conf conf;
-
-  //Input image size
-
-  printf("Entering main controller\n");
-
-  pi_cluster_conf_init(&conf);
-  pi_open_from_conf(&cluster_dev, (void *)&conf);
-  if (pi_cluster_open(&cluster_dev))
-    {
-        printf("Cluster open failed !\n");
-        pmsis_exit(-4);
+  #ifdef __EMUL__
+    S0_Input_1 = AT_L2_ALLOC(0, AT_INPUT_SIZE*sizeof(IMAGE_IN_T));
+    if (S0_Input_1==0){
+      printf("error allocating %ld \n", AT_INPUT_SIZE*sizeof(IMAGE_IN_T));
+      return 1;
     }
-  pi_freq_set(PI_FREQ_DOMAIN_CL,175000000);
-  task = pmsis_l2_malloc(sizeof(struct pi_cluster_task));
-  memset(task, 0, sizeof(struct pi_cluster_task));
-  task->entry = &RunNetwork;
-  task->stack_size = STACK_SIZE;
-  task->slave_stack_size = SLAVE_STACK_SIZE;
-  task->arg = NULL;
+  #else
+    ImageName = __XSTR(AT_IMAGE);
+    pi_freq_set(PI_FREQ_DOMAIN_CL,175000000);
+
+    struct pi_device cluster_dev;
+    struct pi_cluster_conf conf;
+    pi_cluster_conf_init(&conf);
+    pi_open_from_conf(&cluster_dev, (void *)&conf);
+    if (pi_cluster_open(&cluster_dev))
+      {
+          printf("Cluster open failed !\n");
+          pmsis_exit(-4);
+      }
+
+    struct pi_cluster_task *task = pmsis_l2_malloc(sizeof(struct pi_cluster_task));
+    memset(task, 0, sizeof(struct pi_cluster_task));
+    task->entry = &RunNetwork;
+    task->stack_size = STACK_SIZE;
+    task->slave_stack_size = SLAVE_STACK_SIZE;
+    task->arg = NULL;
+    printf("Stack sizes: %d %d\n", STACK_SIZE, SLAVE_STACK_SIZE);
+  #endif
+
+  ResOut = (short int *) AT_L2_ALLOC(0, NUM_CLASSES*sizeof(short int));
+  if (ResOut==0) {
+    printf("Failed to allocate Memory for Result (%ld bytes)\n", NUM_CLASSES*sizeof(short int));
+    return 1;
+  }
 
   printf("Constructor\n");
-
   // IMPORTANT - MUST BE CALLED AFTER THE CLUSTER IS SWITCHED ON!!!!
-  int err;
-  if (err=__PREFIX(CNN_Construct)())
+  if (__PREFIX(CNN_Construct)())
   {
-    printf("Graph constructor exited with %d error\n",err);
-    pmsis_exit(-1);
+    printf("Graph constructor exited with error\n");
+    return 1;
   }
 
-  ImageIn = pmsis_l2_malloc(AT_INPUT_SIZE*sizeof(IMAGE_IN_T));
-  if (ImageIn==0){
-    printf("error allocating %ld \n", AT_INPUT_SIZE*sizeof(IMAGE_IN_T));
-    pmsis_exit(-1);
-  } else {
-    printf("ImageIn address: %ld \n", ImageIn);
-  }
-  
   printf("Reading image\n");
-  //Reading Image from Bridge
   if (ReadImageFromFile(ImageName, AT_INPUT_WIDTH, AT_INPUT_HEIGHT, AT_INPUT_COLORS,
-                        ImageIn, AT_INPUT_SIZE*sizeof(IMAGE_IN_T), IMGIO_OUTPUT_CHAR, 0)) {
+                        S0_Input_1, AT_INPUT_SIZE*sizeof(IMAGE_IN_T), IMGIO_OUTPUT_CHAR, 0)) {
     printf("Failed to load image %s\n", ImageName);
-    pmsis_exit(-1);
-    
+    return 1;
   }
   printf("Finished reading image\n");
 
-  ResOut = (short int *) AT_L2_ALLOC(0, AT_OUTPUT_SIZE*sizeof(short int));
-
-  if (ResOut==0) {
-    printf("Failed to allocate Memory for Result (%ld bytes)\n", 2*sizeof(short int));
-    pmsis_exit(-1);
-  }
-
-  printf("Call cluster\n");
-  // Execute the function "RunNetwork" on the cluster.
-  pi_cluster_send_task_to_cl(&cluster_dev, task);
+  #ifdef __EMUL__
+    RunNetwork();
+  #else
+    pi_cluster_send_task_to_cl(&cluster_dev, task);
+  #endif
   
   __PREFIX(CNN_Destruct)();
-  pi_cluster_close(&cluster_dev);
 
-#ifdef PERF
-	unsigned int TotalCycles = 0, TotalOper = 0;
-	printf("\n");
-	for (int i=0; i<(sizeof(AT_GraphPerf)/sizeof(unsigned int)); i++) {
-		printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", AT_GraphNodeNames[i], AT_GraphPerf[i], AT_GraphOperInfosNames[i], ((float) AT_GraphOperInfosNames[i])/ AT_GraphPerf[i]);
-		TotalCycles += AT_GraphPerf[i]; TotalOper += AT_GraphOperInfosNames[i];
-	}
-	printf("\n");
-	printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", "Total", TotalCycles, TotalOper, ((float) TotalOper)/ TotalCycles);
-	printf("\n");
-#endif
+  #ifdef PERF
+  	unsigned int TotalCycles = 0, TotalOper = 0;
+  	printf("\n");
+  	for (int i=0; i<(sizeof(AT_GraphPerf)/sizeof(unsigned int)); i++) {
+  		printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", AT_GraphNodeNames[i], AT_GraphPerf[i], AT_GraphOperInfosNames[i], ((float) AT_GraphOperInfosNames[i])/ AT_GraphPerf[i]);
+  		TotalCycles += AT_GraphPerf[i]; TotalOper += AT_GraphOperInfosNames[i];
+  	}
+  	printf("\n");
+  	printf("%45s: Cycles: %10d, Operations: %10d, Operations/Cycle: %f\n", "Total", TotalCycles, TotalOper, ((float) TotalOper)/ TotalCycles);
+  	printf("\n");
+  #endif
 
   printf("Ended\n");
-  pmsis_exit(0);
-  return 0;
+  #ifdef __EMUL__
+    return 0;
+  #else
+    pmsis_exit(0);
+  #endif
 }
 
 #ifndef __EMUL__
